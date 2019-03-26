af6762ec8c6ef40efd0f16875b3895e9e90ceec1
XP/GCCVJ <ilab.utk@gmail.com>
2019-03-26 16:59:04 -0400

    Update Commands.py

--- Python/Commands.py
+++ Python/Commands.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="mati.engr.utk.edu",
-  user="rzhou7",
-  passwd="rzhou7@421",
-  database="rzhou7",
+  user="YOURUSERNAME",
+  passwd="YOURPASSWORD",
+  database="YOURSCHEMA",
   port="33060"
 )
 
